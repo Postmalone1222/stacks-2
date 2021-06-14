be74ee689fe2938f8b2562ac040acd67ff67b768
Postmalone1222 <85886635+Postmalone1222@users.noreply.github.com>
2021-06-14 19:49:20 +0530

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -11,3 +11,13 @@ document.body.onkeydown = function( e ) {
         render();
     }
 };
+
+const leftButton = document.getElementById("leftButton");
+const rightButton = document.getElementById("rightButton");
+const upButton = document.getElementById("upButton");
+const downButton = document.getElementById("downButton");
+
+leftButton.onclick = () => changeDirection(LEFT_DIR);
+rightButton.onclick = () => changeDirection(RIGHT_DIR);
+upButton.onclick = () => changeDirection(UP_DIR);
+downButton.onclick = () => changeDirection(DOWN_DIR);
